e069011d56cc16ed463fde3dd8882a5df35d9772
unknown <patrickr.89sc@gmail.com>
2021-12-01 19:23:02 +0100

    everything

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati animi
+        numquam, similique placeat dolorum corrupti perspiciatis recusandae ex.
+        Accusamus, minus? Aliquam iste voluptate amet est inventore vitae
+        corrupti id ipsa! Totam ratione laudantium vitae suscipit, expedita
+        perferendis fugiat, a rem iure, libero quis architecto nobis assumenda?
+        Itaque porro vero nihil? Officiis voluptas quibusdam rerum quia rem
+        itaque dicta similique cum. Repellendus, repudiandae. Saepe id, cumque
+        dolorem ad nulla libero quae. Incidunt culpa blanditiis perspiciatis,
+        fugiat, ex maiores laudantium dolores, molestias iste nam minus? Qui
+        eligendi, obcaecati veniam fugiat adipisci reprehenderit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
